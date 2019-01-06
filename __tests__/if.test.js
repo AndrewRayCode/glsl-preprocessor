023d20cabbbc906eb662166cfc9b784e49f3a582
Andrew Ray <me@andrewray.me>
2019-01-05 18:43:20 -0800

    i have the power of god and anime on my side

--- __tests__/if.test.js
+++ __tests__/if.test.js
@@ -62,4 +62,28 @@ describe('#if', () => {
     const res = new Preprocessor().preprocess(src);
     expect(res.trim()).toEqual('x = 1;');
   });
+
+  it('parses all the things without error', () => {
+    const src = `
+		#if !1 && 2^4 + 60 << 3 + ~1 / 7 * 4%3 +(7 + 3)
+		#endif
+		`;
+    new Preprocessor().preprocess(src);
+  });
+
+  it('does not execute console.log in eval() of expression', () => {
+    const src = `
+		#if console.log('pwned')
+			float x = 1;
+		#endif
+		`;
+    const spy = jest.spyOn(global.console, 'log');
+    try {
+      new Preprocessor().preprocess(src);
+      expect(true).toBe(false);
+    } catch (e) {
+      expect(e).toContain('is not a constant expression');
+      expect(spy).not.toHaveBeenCalled();
+    }
+  });
 });
